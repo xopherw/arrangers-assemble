ee89a0dcb38e374f40759a36b535e193f80705b1
Christopher Wong <31772094+xopherw@users.noreply.github.com>
2021-11-08 21:11:57 +0800

    Add files via upload

---
+++ arrangers assemble.py
@@ -0,0 +1,25 @@
+import os, shutil
+
+path = os.getcwd()
+x = list(os.walk(path))
+
+def isDir(path):
+    return os.path.isdir(path)
+
+def createDir(path):
+    os.mkdir(path)
+
+def isSubtitle(file):
+    return file.endswith((".srt", ".vtt"))
+
+def moveSubtitle(file, path):
+    shutil.move(file, path)
+
+
+for i in x[1:]: # starting 2nd index to check if it is a directory
+    subFolder = i[0] + "/" + "Subtitles"
+    createDir(subFolder)
+    for j in i[2:]:
+        for k in j:
+            if(isSubtitle(k)):
+                moveSubtitle(i[0] + "/" + k, subFolder)
