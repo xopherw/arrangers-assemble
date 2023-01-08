2eacd3f5e346006e5c6ccc0b6bf968dedf693e43
xopherw <chriswwl95@gmail.com>
2023-01-08 14:17:41 +1100

    change check folder logic

--- arrangers assemble.py
+++ arrangers assemble.py
@@ -18,8 +18,11 @@ def moveSubtitle(file, path):
 
 for i in x[1:]: # starting 2nd index to check if it is a directory
     subFolder = i[0] + "/" + "Subtitles"
-    createDir(subFolder)
-    for j in i[2:]:
-        for k in j:
-            if(isSubtitle(k)):
-                moveSubtitle(i[0] + "/" + k, subFolder)
+    if(os.path.exists(subFolder)):
+        pass
+    else:
+        createDir(subFolder)
+        for j in i[2:]:
+            for k in j:
+                if(isSubtitle(k)):
+                    moveSubtitle(i[0] + "/" + k, subFolder)
